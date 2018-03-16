1d8ac178fe1f69a621eb99dbc474e4008ff97b12
Bonnie Zhou <bzbee003@gmail.com>
2018-03-15 17:39:31 -0700

    Create React app that says "Hello World!" (#5)

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
+
+export default App;
