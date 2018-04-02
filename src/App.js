6c939f0240d32ff6f785be743a86bf50c328453b
Matt Goo <mattgoo@google.com>
2018-04-02 15:33:13 -0700

    WIP: added eslintrc file

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.scss';
 class App extends Component {
   render() {
     return (
-      <header className="mdc-top-app-bar">
-        <div className="mdc-top-app-bar__row">
-          <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
-            <span className="mdc-top-app-bar__title">Material Components Web | Catalog</span>
+      <header className='mdc-top-app-bar'>
+        <div className='mdc-top-app-bar__row'>
+          <section className='mdc-top-app-bar__section mdc-top-app-bar__section--align-start'>
+            <span className='mdc-top-app-bar__title'>Material Components Web | Catalog</span>
           </section>
         </div>
       </header>
