d17dda1afc1b01e1a4eec3f848ab3c9742e7a52c
Bonnie Zhou <bonniez@google.com>
2018-04-02 14:48:25 -0700

    Add vanilla top app bar component

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -192,7 +192,7 @@ module.exports = {
                   importer: function(url, prev) {
                     if(url.indexOf('@material') === 0) {
                       var filePath = url.split('@material')[1];
-                      var nodeModulePath = `../../node_modules/@material/${filePath}`;
+                      var nodeModulePath = `node_modules/@material/${filePath}`;
                       return { file: require('path').resolve(nodeModulePath) };
                     }
                     return { file: url };
--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.scss';
 class App extends Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <header className="mdc-top-app-bar">
+        <div className="mdc-top-app-bar__row">
+          <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+            <span className="mdc-top-app-bar__title">Material Components Web | Catalog</span>
+          </section>
+        </div>
+      </header>
     );
   }
 }
