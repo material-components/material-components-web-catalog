8efec956e6ad6d6a831aafcf2476e10d24c84676
Bonnie Zhou <bonniez@google.com>
2018-04-04 13:25:09 -0700

    Add template for component page and use in button page

--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import HeaderBar from './HeaderBar.js';
-import Hero from './Hero.js';
+import ComponentPage from './ComponentPage.js';
 
 import {MDCRipple} from '@material/ripple';
 
@@ -30,12 +30,91 @@ class ButtonHero extends Component {
   }
 }
 
+class ButtonDemos extends Component {
+  componentDidMount() {
+    const buttons = document.querySelectorAll('.mdc-button');
+    buttons.forEach(button => new MDCRipple(button));
+  }
+
+  render() {
+    return (
+      <div>
+        <h3 className='mdc-typography--subheading2'>Text Button</h3>
+        <div className='button-wrapper'>
+          <button className='mdc-button'>
+            Default
+          </button>
+          <button className='mdc-button mdc-button--dense'>
+            Dense
+          </button>
+          <button className='mdc-button'>
+            <i className='material-icons mdc-button__icon'>favorite</i>
+            Icon
+          </button>
+        </div>
+
+        <h3 className='mdc-typography--subheading2'>Raised Button</h3>
+        <div className='button-wrapper'>
+          <button className='mdc-button mdc-button--raised'>
+            Default
+          </button>
+          <button className='mdc-button mdc-button--raised mdc-button--dense'>
+            Dense
+          </button>
+          <button className='mdc-button mdc-button--raised'>
+            <i className='material-icons mdc-button__icon'>favorite</i>
+            Icon
+          </button>
+        </div>
+
+        <h3 className='mdc-typography--subheading2'>Unelevated Button</h3>
+        <div className='button-wrapper'>
+          <button className='mdc-button mdc-button--unelevated'>
+            Default
+          </button>
+          <button className='mdc-button mdc-button--unelevated mdc-button--dense'>
+            Dense
+          </button>
+          <button className='mdc-button mdc-button--unelevated'>
+            <i className='material-icons mdc-button__icon'>favorite</i>
+            Icon
+          </button>
+        </div>
+
+        <h3 className='mdc-typography--subheading2'>Unelevated Button</h3>
+        <div className='button-wrapper'>
+          <button className='mdc-button mdc-button--stroked'>
+            Default
+          </button>
+          <button className='mdc-button mdc-button--stroked mdc-button--dense'>
+            Dense
+          </button>
+          <button className='mdc-button mdc-button--stroked'>
+            <i className='material-icons mdc-button__icon'>favorite</i>
+            Icon
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
 class Button extends Component {
   render() {
     return (
       <div>
         <HeaderBar title='Button'/>
-        <Hero children={<ButtonHero/>}/>
+        <ComponentPage
+          hero={<ButtonHero/>}
+          sidebarComponentSelector='.sidebar-buttons'
+          title='Buttons'
+          description='Buttons communicate an action a user can take. They are typically placed throughout your UI, in places
+          like dialogs, forms, cards, and toolbars.'
+          designLink='https://material.io/guidelines/components/buttons.html'
+          docsLink='https://material.io/components/web/catalog/buttons/'
+          sourceLink='https://github.com/material-components/material-components-web/tree/master/packages/mdc-button'
+          demos={<ButtonDemos/>}
+        />
       </div>
     );
   }
---
+++ src/ComponentPage.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class ComponentPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {sidebarComponentSelector: props.sidebarComponentSelector};
+  }
+
+  componentDidMount() {
+    const activeComponent = document.querySelector(this.state.sidebarComponentSelector);
+    activeComponent.classList.add('sidebar-active');
+  }
+
+  render() {
+    return (
+      <div>
+        <section className='hero'>
+          {this.props.hero}
+        </section>
+
+        <div className='mdc-layout-grid'>
+          <div className='mdc-layout-grid__inner'>
+            <section className='sidebar mdc-layout-grid__cell mdc-layout-grid__cell--span-2'>
+              <a href='/button.html' role='listitem' className='sidebar-buttons mdc-list-item'>Buttons</a>
+              <a href='/card.html' role='listitem' className='sidebar-cards mdc-list-item'>Cards</a>
+            </section>
+
+            <section className='demo-wrapper mdc-layout-grid__cell mdc-layout-grid__cell--span-10'>
+              <h1 className='mdc-typography--headline'>{this.props.title}</h1>
+              <p className='mdc-typography--body1'>
+                {this.props.description}
+              </p>
+
+              <h2 className='mdc-typography--title'>Resources</h2>
+              <a href={this.props.designLink} role='listitem' className='mdc-list-item' target='_blank'>
+                <span className='mdc-list-item__graphic'><img src='material_design_black_24dp.png'/></span>
+                <span className='mdc-list-item__text'>Material Design Guidelines</span>
+              </a>
+              <a href={this.props.docsLink} role='listitem' className='mdc-list-item' target='_blank'>
+                <span className='mdc-list-item__graphic'><img src='drive_document_black_24dp.png'/></span>
+                <span className='mdc-list-item__text'>Documentation</span>
+              </a>
+              <a href={this.props.sourceLink} role='listitem' className='mdc-list-item' target='_blank'>
+                <span className='mdc-list-item__graphic'><img src='code_black_24dp.png'/></span>
+                <span className='mdc-list-item__text'>Source Code</span>
+              </a>
+
+              <h2 className='mdc-typography--title'>Demos</h2>
+              {this.props.demos}
+            </section>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ComponentPage;
