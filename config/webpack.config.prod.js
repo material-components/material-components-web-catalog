5bbb7c99faa0ed126838c996dfdfa76eef3d4513
Matt Goo <mattgoo@google.com>
2018-04-09 14:13:42 -0700

    chore: Use `includePaths` instead of `importer` in `sass-loader` config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -93,7 +93,7 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -124,7 +124,7 @@ module.exports = {
             options: {
               formatter: eslintFormatter,
               eslintPath: require.resolve('eslint'),
-              
+
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -152,7 +152,7 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              
+
               compact: true,
             },
           },
@@ -187,7 +187,7 @@ module.exports = {
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
                       },
-                    },                   
+                    },
                     {
                       loader: require.resolve('postcss-loader'),
                       options: {
@@ -211,14 +211,7 @@ module.exports = {
                     {
                       loader: 'sass-loader',
                       options: {
-                        importer: function(url, prev) {
-                          if(url.indexOf('@material') === 0) {
-                            var filePath = url.split('@material')[1];
-                            var nodeModulePath = `node_modules/@material/${filePath}`;
-                            return { file: require('path').resolve(nodeModulePath) };
-                          }
-                          return { file: url };
-                        }
+                        includePaths: ['./node_modules']
                       }
                     },
                   ],
