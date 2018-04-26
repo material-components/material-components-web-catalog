18c06f6223f3ca244a039bf7768a19ce6400126b
Kenneth G. Franqueiro <kfranqueiro@google.com>
2018-04-26 16:14:21 -0400

    Replace button icon with image

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
       <div>
         <HeaderBar isTopPage />
         <ul id='catalog-image-list' className='mdc-image-list standard-image-list mdc-top-app-bar--fixed-adjust'>
-          {this.renderListItem('Button', `${imagePath}/ic_button_24px.svg`, 'button')}
+          {this.renderListItem('Button', `${imagePath}/buttons_180px.svg`, 'button')}
         </ul>
       </div>
     );
