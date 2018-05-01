fe549dbde2a7002b7f32db0702262bf6eeb75921
Matt Goo <mattgoo@google.com>
2018-05-01 11:31:00 -0700

    feature(list): changed class -> functional components

--- src/ListPage.js
+++ src/ListPage.js
@@ -2,54 +2,43 @@ import React, { Component } from 'react';
 import ComponentPage from './ComponentPage.js';
 import HeaderBar from './HeaderBar.js';
 import {MDCRipple} from '@material/ripple';
+import classnames from 'classnames';
 
 import './styles/ListPage.scss';
 
-const ListPage = () => {
-  return (
-    <div>
-      <HeaderBar />
-      <ComponentPage
-        hero={<ListHero/>}
-        title='List'
-        description='Lists present multiple line items vertically as a single continuous element.'
-        designLink='https://material.io/guidelines/components/lists.html'
-        docsLink='https://material.io/components/web/catalog/lists/'
-        sourceLink='https://github.com/material-components/material-components-web/tree/master/packages/mdc-list'
-        demos={<ListDemos/>}
-      />
-    </div>
-  );
-}
+const ListPage = () => (
+  <div>
+    <HeaderBar />
+    <ComponentPage
+      hero={<ListHero/>}
+      title='List'
+      description='Lists present multiple line items vertically as a single continuous element.'
+      designLink='https://material.io/guidelines/components/lists.html'
+      docsLink='https://material.io/components/web/catalog/lists/'
+      sourceLink='https://github.com/material-components/material-components-web/tree/master/packages/mdc-list'
+      demos={<ListDemos/>}
+    />
+  </div>
+);
 
-class ListHero extends Component {
-  render() {
-    return (
-      <div className='hero-list'>
-        <List>
-          <ListItem lineOne='Line item' />
-          <ListItem lineOne='Line item' />
-          <ListItem lineOne='Line item' />
-        </List>
-      </div>
-    )
-  }
-}
+const ListHero = () => (
+  <div className='hero-list'>
+    <List>
+      <ListItem lineOne='Line item' />
+      <ListItem lineOne='Line item' />
+      <ListItem lineOne='Line item' />
+    </List>
+  </div>
+);
 
-class ListDivider extends Component {
-  render() {
-    return (
-      <li className='mdc-list-divider' role='separator'></li>
-    )
-  }
-}
+const ListDivider = () => (
+  <li className='mdc-list-divider' role='separator'></li>
+);
 
 class ListItem extends Component {
-  constructor(props) {
-    super(props);
-    this.ripple = null;
-    this.initRipple = (surface) => this.ripple = new MDCRipple(surface);
-  }
+
+  ripple = null;
+  initRipple = (surface) => this.ripple = new MDCRipple(surface);
 
   componentWillUnmount() {
     this.ripple.destroy();
@@ -59,7 +48,7 @@ class ListItem extends Component {
     if (this.props.leadingIcon) {
       return (
         <span className='mdc-list-item__graphic material-icons' aria-hidden='true'>{this.props.leadingIcon}</span>
-      )
+      );
     }
   }
 
@@ -67,7 +56,7 @@ class ListItem extends Component {
     if (this.props.lineTwo) {
       return (
         <span className='mdc-list-item__secondary-text'>{this.props.lineTwo}</span>
-      )
+      );
     }
   }
 
@@ -75,7 +64,7 @@ class ListItem extends Component {
     if (this.props.trailingIcon) {
       return (
         <span className='mdc-list-item__meta material-icons' aria-hidden='true'>{this.props.trailingIcon}</span>
-      )
+      );
     }
   }
 
@@ -95,66 +84,60 @@ class ListItem extends Component {
 
 class List extends Component {
   render() {
+    const classes = classnames('mdc-list demo-list', {
+      'mdc-list--dense': this.props.dense,
+      'mdc-list--two-line': this.props.twoLines,
+      'mdc-list--avatar-list': this.props.avatars,
+    });
     return (
-      <ul className={`mdc-list demo-list
-                      ${this.props.dense && 'mdc-list--dense'}
-                      ${this.props.twoLines && 'mdc-list--two-line'}
-                      ${this.props.avatars && 'mdc-list--avatar-list'}`}>
+      <ul className={classes}>
         {this.props.children}
       </ul>
     );
   }
 }
 
-class ListVariant extends Component {
-  render() {
-    return (
-      <div>
-        <h3 className='mdc-typography--subheading2'>{this.props.title}</h3>
-        <List {...this.props} />
-      </div>
-    )
-  }
-}
+const ListVariant = (props) => (
+  <div>
+    <h3 className='mdc-typography--subheading2'>{props.title}</h3>
+    <List {...props} />
+  </div>
+);
 
-class ListDemos extends Component {
-  render() {
-    return (
-      <div>
-        <ListVariant title='Single-Line'>
-          <ListItem lineOne='Line item' />
-          <ListItem lineOne='Line item' />
-          <ListItem lineOne='Line item' />
-        </ListVariant>
+const ListDemos = () => (
+  <div>
+    <ListVariant title='Single-Line'>
+      <ListItem lineOne='Line item' />
+      <ListItem lineOne='Line item' />
+      <ListItem lineOne='Line item' />
+    </ListVariant>
 
-        <ListVariant title='Two-Line' twoLines>
-          <ListItem lineOne='Line item' lineTwo='Secondary text' />
-          <ListItem lineOne='Line item' lineTwo='Secondary text' />
-          <ListItem lineOne='Line item' lineTwo='Secondary text' />
-        </ListVariant>
+    <ListVariant title='Two-Line' twoLines>
+      <ListItem lineOne='Line item' lineTwo='Secondary text' />
+      <ListItem lineOne='Line item' lineTwo='Secondary text' />
+      <ListItem lineOne='Line item' lineTwo='Secondary text' />
+    </ListVariant>
 
-        <ListVariant title='Leading Icon'>
-          <ListItem lineOne='Line item' leadingIcon='wifi' />
-          <ListItem lineOne='Line item' leadingIcon='bluetooth' />
-          <ListItem lineOne='Line item' leadingIcon='data_usage' />
-        </ListVariant>
+    <ListVariant title='Leading Icon'>
+      <ListItem lineOne='Line item' leadingIcon='wifi' />
+      <ListItem lineOne='Line item' leadingIcon='bluetooth' />
+      <ListItem lineOne='Line item' leadingIcon='data_usage' />
+    </ListVariant>
 
-        <ListVariant title='Trailing Icon'>
-          <ListItem lineOne='Line item' trailingIcon='info' />
-          <ListItem lineOne='Line item' trailingIcon='info' />
-          <ListItem lineOne='Line item' trailingIcon='info' />
-        </ListVariant>
+    <ListVariant title='Trailing Icon'>
+      <ListItem lineOne='Line item' trailingIcon='info' />
+      <ListItem lineOne='Line item' trailingIcon='info' />
+      <ListItem lineOne='Line item' trailingIcon='info' />
+    </ListVariant>
 
-        <ListVariant title='Two-Line with Leading and Trailing Icon and Divider' twoLines avatars>
-          <ListItem lineOne='Dog Photos' lineTwo='9 Jan 2018' leadingIcon='folder' trailingIcon='info' />
-          <ListItem lineOne='Cat Photos' lineTwo='22 Dec 2017' leadingIcon='folder' trailingIcon='info' />
-          <ListDivider />
-          <ListItem lineOne='Potatoes' lineTwo='30 Noc 2017' leadingIcon='folder' trailingIcon='info' />
-          <ListItem lineOne='Carrots' lineTwo='17 Oct 2017' leadingIcon='folder' trailingIcon='info' />
-        </ListVariant>
-      </div>
-    )
-  }
-}
+    <ListVariant title='Two-Line with Leading and Trailing Icon and Divider' twoLines avatars>
+      <ListItem lineOne='Dog Photos' lineTwo='9 Jan 2018' leadingIcon='folder' trailingIcon='info' />
+      <ListItem lineOne='Cat Photos' lineTwo='22 Dec 2017' leadingIcon='folder' trailingIcon='info' />
+      <ListDivider />
+      <ListItem lineOne='Potatoes' lineTwo='30 Noc 2017' leadingIcon='folder' trailingIcon='info' />
+      <ListItem lineOne='Carrots' lineTwo='17 Oct 2017' leadingIcon='folder' trailingIcon='info' />
+    </ListVariant>
+  </div>
+);
 
 export default ListPage;
