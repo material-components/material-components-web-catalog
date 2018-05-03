0508ff08474def96394c38561c17ad5d13bb2bd5
Will Ernest <34519388+williamernest@users.noreply.github.com>
2018-05-02 18:51:04 -0700

    Add lorem ipsum to iframe pages (#95)

--- src/App.js
+++ src/App.js
@@ -36,7 +36,6 @@ const componentUrlToPageMap = {
   '/drawer/temporary': <DrawerFramePage type='temporary' />,
   '/drawer/permanent': <DrawerFramePage type='permanent' />,
   '/drawer/persistent': <DrawerFramePage type='persistent' />,
-  '/drawer/permanentBelowTopAppBar': <DrawerFramePage type='permanentBelowTopAppBar' />,
   '/elevation': <ElevationPage />,
   '/fab': <FabPage />,
   '/icon-toggle': <IconTogglePage />,
