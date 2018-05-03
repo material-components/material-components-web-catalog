a726090f39db98498dd98fce6e3315128e8e55d7
Matt Goo <mattgoo@google.com>
2018-05-03 10:34:02 -0700

    fix: Scroll app to top of page on route change (#109)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {unregister} from './registerServiceWorker';
+import {HashRouter} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <HashRouter>
+    <App />
+  </HashRouter>
+), document.getElementById('root'));
+
 unregister();
