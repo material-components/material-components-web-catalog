1f8ee1af8f536cca6aeccd83a67f115fee515dce
Will Ernest <34519388+williamernest@users.noreply.github.com>
2018-05-09 16:42:50 -0700

    Modify header text to be abbreviated in mobile (#150)

--- src/HeaderBar.js
+++ src/HeaderBar.js
@@ -17,7 +17,12 @@ class HeaderBar extends Component {
                 className='mdc-top-app-bar__section mdc-top-app-bar__section--align-start'>
               <HeaderIcon toggleDrawer={this.props.toggleDrawer} isTopPage={this.props.isTopPage}/>
               <span className='mdc-top-app-bar__title catalog-top-app-bar__title'>
-              Material Components for the Web
+                <span className='catalog-top-app-bar__title--small-screen'>
+                  MDC Web
+                </span>
+                <span className='catalog-top-app-bar__title--large-screen'>
+                  Material Components for the Web
+                </span>
               </span>
             </section>
           </div>
