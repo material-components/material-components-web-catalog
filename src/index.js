aab32e5994eb5b23d5787cfdecf5d538c30d4075
Will Ernest <williamernest@google.com>
2018-05-14 09:06:47 -0700

    feat(catalog): Add analytics

--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {unregister} from './registerServiceWorker';
 import {HashRouter} from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-118996389-1');
+
+const history = createHistory();
+history.listen((location) => {
+  ReactGA.set({ page: location.hash });
+  ReactGA.pageview(location.hash);
+});
 
 ReactDOM.render((
-  <HashRouter>
+  <HashRouter onUpdate={history}>
     <App />
   </HashRouter>
 ), document.getElementById('root'));
