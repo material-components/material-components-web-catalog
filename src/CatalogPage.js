74f7bca430f48965f5ad8e793d5a3bd5cb38264b
Abhinay Omkar <abhiomkar@gmail.com>
2018-08-28 15:17:20 -0400

    Added new navigation drawer demo & updated catalog sidebar to use new drawer (#190)

--- src/CatalogPage.js
+++ src/CatalogPage.js
@@ -5,23 +5,26 @@ import ComponentImageList from './ComponentImageList';
 import HeaderBar from './HeaderBar';
 
 import {Switch, Route} from 'react-router';
+import './styles/CatalogPage.scss';
 
 // This is a separate page (Not App.js), since Drawer and TopAppBar
 // need to be separate pages due to iFrames.
 class CatalogPage extends React.Component {
   state = {
-    isDrawerOpen: false
+    isDrawerOpen: false,
+    scrollTargetSetter: () => {},
   };
 
   render() {
     return (
-        <div>
-          <HeaderBar toggleDrawer={() => this.setState(
-              {isDrawerOpen: !this.state.isDrawerOpen})}
-                     isTopPage={this.props.location.pathname === '/'}/>
+        <div className='catalog-page-container'>
+          <HeaderBar scrollTarget={(scrollTargetSetter) => this.setState({scrollTargetSetter}) }
+              toggleDrawer={() => this.setState({isDrawerOpen: !this.state.isDrawerOpen})}
+              isTopPage={this.props.location.pathname === '/'}
+          />
           <Switch>
             <Route exact path='/' render={(props) => <ComponentImageList {...props} />}/>
-            <Route path='/component' render={(props) => <ComponentPage {...props} isDrawerOpen={this.state.isDrawerOpen}/>} />
+            <Route path='/component' render={(props) => <ComponentPage {...props} scrollTargetSetter={this.state.scrollTargetSetter} isDrawerOpen={this.state.isDrawerOpen}/>} />
           </Switch>
         </div>
     );
