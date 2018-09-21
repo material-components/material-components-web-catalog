8245efe653391f5b8d97de3209bcd0b8e5b20d24
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2018-09-21 14:02:41 -0400

    feat(dialog): Update to 0.40.0 Dialog (#203)

--- src/DialogCatalog.js
+++ src/DialogCatalog.js
@@ -32,28 +32,26 @@ class DialogHero extends Component {
   render() {
     return (
       <div>
-        <aside id='my-mdc-dialog'
-              className='mdc-dialog mdc-dialog--open hero-demo'
-              role='alertdialog'
-              aria-labelledby='my-mdc-dialog-label'
-              aria-describedby='my-mdc-dialog-description'
-              ref={this.initDialog}
-              >
-          <div className='mdc-dialog__surface'>
-            <header className='mdc-dialog__header'>
-              <h2 id='my-mdc-dialog-label' className='mdc-dialog__header__title'>
-                Get this party started?
-              </h2>
-            </header>
-            <section id='my-mdc-dialog-description' className='mdc-dialog__body'>
-              Turn up the jams and have a good time.
-            </section>
-            <footer className='mdc-dialog__footer'>
-              <button type='button' className='mdc-button mdc-dialog__footer__button mdc-dialog__footer__button--cancel'>Decline</button>
-              <button type='button' className='mdc-button mdc-dialog__footer__button mdc-dialog__footer__button--accept'>Accept</button>
-            </footer>
+        <div id='my-mdc-dialog'
+          className='mdc-dialog mdc-dialog--open hero-demo'
+          role='alertdialog'
+          aria-labelledby='my-mdc-dialog-label'
+          aria-describedby='my-mdc-dialog-description'
+          ref={this.initDialog}
+          >
+          <div className='mdc-dialog__container'>
+            <div className='mdc-dialog__surface'>
+              <h2 id='my-mdc-dialog-label' className='mdc-dialog__title'>Get this party started?</h2>
+              <section id='my-mdc-dialog-description' className='mdc-dialog__content'>
+                Turn up the jams and have a good time.
+              </section>
+              <footer className='mdc-dialog__actions'>
+                <button type='button' className='mdc-button mdc-dialog__button'>Decline</button>
+                <button type='button' className='mdc-button mdc-dialog__button'>Accept</button>
+              </footer>
+            </div>
           </div>
-        </aside>
+        </div>
       </div>
     )
   }
@@ -62,20 +60,15 @@ class DialogHero extends Component {
 class DialogDemos extends Component {
   state = {open: false, hasBeenOpened: false, accepted: false};
 
-  handleAccept_ = () => this.handleAccept();
-  handleCancel_ = () => this.handleCancel();
-  handleOpenClick_ = () => this.handleOpenClick();
+  handleClosing_ = this.handleClosing.bind(this);
+  handleOpenClick_ = this.handleOpenClick.bind(this);
 
   handleOpenClick() {
     this.setState({open: true});
   }
 
-  handleAccept() {
-    this.setState({open: false, accepted: true, hasBeenOpened: true});
-  }
-
-  handleCancel() {
-    this.setState({open: false, accepted: false, hasBeenOpened: true});
+  handleClosing(event) {
+    this.setState({open: false, accepted: event.detail.action !== 'close', hasBeenOpened: true});
   }
 
   renderMessage() {
@@ -93,7 +86,7 @@ class DialogDemos extends Component {
     return (
       <div>
         <h3 className='mdc-typography--subtitle1'>Scrollable Dialog</h3>
-        <Dialog open={this.state.open} handleAccept={this.handleAccept_} handleCancel={this.handleCancel_}/>
+        <Dialog open={this.state.open} handleClosing={this.handleClosing_}/>
         <button className='mdc-button' onClick={this.handleOpenClick_}>Open dialog</button>
         {this.renderMessage()}
       </div>
@@ -102,8 +95,7 @@ class DialogDemos extends Component {
 }
 
 class Dialog extends Component {
-  handleAccept_ = () => this.handleAccept();
-  handleCancel_ = () => this.handleCancel();
+  handleClosing_ = this.handleClosing.bind(this);
   initDialog = (dialogEl) => {
     if (!dialogEl) return;
     this.dialogEl = dialogEl;
@@ -111,66 +103,57 @@ class Dialog extends Component {
   };
 
   componentDidUpdate(prevProps) {
-    if (prevProps.open !== this.props.open) {
-      !!this.props.open
-        ? this.dialog.show()
-        : this.dialog.close();
+    if (!prevProps.open && this.props.open) {
+      // Handle transitioning from closed to open; dialog will handle closing itself
+      this.dialog.open();
     }
   }
 
   componentDidMount() {
-    this.dialogEl.addEventListener('MDCDialog:accept', this.handleAccept_);
-    this.dialogEl.addEventListener('MDCDialog:cancel', this.handleCancel_);
+    this.dialogEl.addEventListener('MDCDialog:closing', this.handleClosing_);
   }
 
   componentWillUnmount() {
-    this.dialogEl.removeEventListener('MDCDialog:accept', this.handleAccept_);
-    this.dialogEl.removeEventListener('MDCDialog:cancel', this.handleCancel_);
+    this.dialogEl.removeEventListener('MDCDialog:closing', this.handleClosing_);
     this.dialog.destroy();
   }
 
-  handleAccept() {
-    this.props.handleAccept();
-  }
-
-  handleCancel() {
-    this.props.handleCancel();
+  handleClosing(event) {
+    this.props.handleClosing(event);
   }
 
   render() {
     return (
       <div>
-        <aside id='mdc-dialog-with-list'
-              className='mdc-dialog'
-              role='alertdialog'
-              aria-labelledby='mdc-dialog-with-list-label'
-              aria-describedby='mdc-dialog-with-list-description'
-              ref={this.initDialog}>
-          <div className='mdc-dialog__surface'>
-            <header className='mdc-dialog__header'>
-              <h2 id='mdc-dialog-with-list-label' className='mdc-dialog__header__title'>
-                Read my novel?
-              </h2>
-            </header>
-            <section id='mdc-dialog-with-list-description' className='mdc-dialog__body mdc-dialog__body--scrollable'>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus, incidunt. Debitis, repudiandae dignissimos et quam velit autem mollitia tenetur, eligendi rerum repellendus, explicabo ad aperiam vel ipsam! Exercitationem, voluptates molestiae.</p>
-              <p>Iusto reiciendis mollitia ab commodi. Animi maiores nesciunt officia enim corrupti officiis consequuntur vel, consectetur eveniet ad dolorum reprehenderit similique qui deleniti ut sed explicabo id error at. Laudantium, excepturi!</p>
-              <p>Suscipit quam laboriosam animi quasi similique voluptatem molestiae voluptas sint itaque, labore eos, maiores harum qui totam libero amet nisi? Similique nihil veritatis aspernatur molestias accusantium, eius dolorum autem optio?</p>
-              <p>Cum eligendi consequuntur voluptas. Repellat nisi commodi numquam aliquam quasi tenetur obcaecati, animi cum eum. Facilis esse cupiditate fugiat, quod eveniet, inventore impedit nam ex tempore harum laudantium provident assumenda.</p>
-              <p>Ut iste aperiam excepturi rerum consectetur illo officiis quo sed sunt labore earum soluta tempore omnis a, enim maiores non? Facilis qui alias sunt veniam esse hic. Aut, ducimus aliquid!</p>
-              <p>Qui quaerat saepe sunt earum nihil porro, sint quibusdam, id eos vero asperiores dolorem iusto dolore illo, architecto fuga? Voluptates distinctio eligendi nihil provident accusantium. Maxime ullam ratione officia non.</p>
-              <p>Molestiae sapiente quae nulla. Voluptates quibusdam numquam earum vero deserunt in, cum tenetur accusamus ipsum minus veniam libero quasi fuga dolorem laudantium error quo et accusantium neque vitae aliquam tempore.</p>
-              <p>Optio asperiores quisquam odit eaque incidunt laboriosam repudiandae ex eum iure quia, id vero atque perspiciatis, officiis quaerat aut ut dolorem libero eos perferendis ducimus! Veritatis nam libero tempora maxime?</p>
-              <p>Sapiente reiciendis quis eveniet iure dicta perferendis quos consectetur, soluta sunt, labore ipsam inventore maiores laudantium recusandae deleniti autem animi consequatur, voluptatem sint. Dignissimos minima labore earum vitae ad non!</p>
-              <p>Cum ex totam dolore officiis maiores quidem necessitatibus consequatur molestias culpa, quas, aperiam tempora et! Dolorem, voluptates dignissimos? Voluptatem voluptatibus expedita, error ducimus distinctio necessitatibus laudantium officiis dolorum nam vitae?</p>
-            </section>
-            <footer className='mdc-dialog__footer'>
-              <button type='button' className='mdc-button mdc-dialog__footer__button mdc-dialog__footer__button--cancel'>Decline</button>
-              <button type='button' className='mdc-button mdc-dialog__footer__button mdc-dialog__footer__button--accept'>Accept</button>
-            </footer>
+        <div id='mdc-dialog-with-list'
+          className='mdc-dialog'
+          role='alertdialog'
+          aria-labelledby='mdc-dialog-with-list-label'
+          aria-describedby='mdc-dialog-with-list-description'
+          ref={this.initDialog}>
+          <div className='mdc-dialog__scrim'></div>
+          <div className='mdc-dialog__container'>
+            <div className='mdc-dialog__surface'>
+              <h2 id='mdc-dialog-with-list-label' className='mdc-dialog__title'>Read my novel?</h2>
+              <section id='mdc-dialog-with-list-description' className='mdc-dialog__content'>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus, incidunt. Debitis, repudiandae dignissimos et quam velit autem mollitia tenetur, eligendi rerum repellendus, explicabo ad aperiam vel ipsam! Exercitationem, voluptates molestiae.</p>
+                <p>Iusto reiciendis mollitia ab commodi. Animi maiores nesciunt officia enim corrupti officiis consequuntur vel, consectetur eveniet ad dolorum reprehenderit similique qui deleniti ut sed explicabo id error at. Laudantium, excepturi!</p>
+                <p>Suscipit quam laboriosam animi quasi similique voluptatem molestiae voluptas sint itaque, labore eos, maiores harum qui totam libero amet nisi? Similique nihil veritatis aspernatur molestias accusantium, eius dolorum autem optio?</p>
+                <p>Cum eligendi consequuntur voluptas. Repellat nisi commodi numquam aliquam quasi tenetur obcaecati, animi cum eum. Facilis esse cupiditate fugiat, quod eveniet, inventore impedit nam ex tempore harum laudantium provident assumenda.</p>
+                <p>Ut iste aperiam excepturi rerum consectetur illo officiis quo sed sunt labore earum soluta tempore omnis a, enim maiores non? Facilis qui alias sunt veniam esse hic. Aut, ducimus aliquid!</p>
+                <p>Qui quaerat saepe sunt earum nihil porro, sint quibusdam, id eos vero asperiores dolorem iusto dolore illo, architecto fuga? Voluptates distinctio eligendi nihil provident accusantium. Maxime ullam ratione officia non.</p>
+                <p>Molestiae sapiente quae nulla. Voluptates quibusdam numquam earum vero deserunt in, cum tenetur accusamus ipsum minus veniam libero quasi fuga dolorem laudantium error quo et accusantium neque vitae aliquam tempore.</p>
+                <p>Optio asperiores quisquam odit eaque incidunt laboriosam repudiandae ex eum iure quia, id vero atque perspiciatis, officiis quaerat aut ut dolorem libero eos perferendis ducimus! Veritatis nam libero tempora maxime?</p>
+                <p>Sapiente reiciendis quis eveniet iure dicta perferendis quos consectetur, soluta sunt, labore ipsam inventore maiores laudantium recusandae deleniti autem animi consequatur, voluptatem sint. Dignissimos minima labore earum vitae ad non!</p>
+                <p>Cum ex totam dolore officiis maiores quidem necessitatibus consequatur molestias culpa, quas, aperiam tempora et! Dolorem, voluptates dignissimos? Voluptatem voluptatibus expedita, error ducimus distinctio necessitatibus laudantium officiis dolorum nam vitae?</p>
+              </section>
+              <footer className='mdc-dialog__actions'>
+                <button type='button' className='mdc-button mdc-dialog__button' data-mdc-dialog-action='close'>Decline</button>
+                <button type='button' className='mdc-button mdc-dialog__button' data-mdc-dialog-action='accept'>Accept</button>
+              </footer>
+            </div>
           </div>
-          <div className='mdc-dialog__backdrop'></div>
-        </aside>
+        </div>
       </div>
     );
   }
