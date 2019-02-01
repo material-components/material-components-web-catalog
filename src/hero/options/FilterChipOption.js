bb64f30f556d67fc0592efb2160f6b214b258db0
Yasmine Evjen <yasmineevjen@users.noreply.github.com>
2019-02-01 18:00:47 -0500

    Hero updates
    Added Material Icons instructions link and overline padding.

--- src/hero/options/FilterChipOption.js
+++ src/hero/options/FilterChipOption.js
@@ -39,7 +39,7 @@ export default class FilterChipOption extends Component {
     } = this.props;
 
     if (!this.isMounted_) return null;
-    
+
     return (
       <React.Fragment>
         <li className='mdc-list-item'>
@@ -50,6 +50,7 @@ export default class FilterChipOption extends Component {
         <li className='mdc-list-item'>
           <span className='mdc-typography--body2'>
             {optionDescription}
+            Follow the <a href={'http://google.github.io/material-design-icons/'}>instructions</a> to embed the icon font in your site.
           </span>
         </li>
         <li className='mdc-list-item'>
