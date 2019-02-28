cb0de981444c8b7b7ab62b61a7940e46c6b5ec98
Matt Goo <mattgoo@google.com>
2019-02-27 16:59:01 -0800

    fix: return empty array

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,10 +1,10 @@
 // TODO(mattgoo): remove this once react chips is updated.
 // Array.from is not supported by IE11 and is in react-chips/react-ripple.
 // https://github.com/material-components/material-components-web-react/issues/700
-// Production steps of ECMA-262, Edition 6, 22.1.2.1
+
 if (!Array.from) {
   Array.from = function (object) {
-      'use strict';
-      return [].slice.call(object);
+    if (!object) return [];
+    return [].slice.call(object);
   };
 }
